21c6065a1570ef7917db844f18ac6f6b8dc252a8
noorshah <noor.sabah1988@gmail.com>
2020-06-13 00:14:20 +0100

    basic setup of typescript cucumberJS and webdriverIO

---
+++ features/GoogleSearch.feature
@@ -0,0 +1,6 @@
+Feature: Google search
+
+   Scenario: Google Search
+      Given I navigate to google home page
+      When I search for mangoes 
+      Then I get relevant information
---
+++ step-definitions/google-search-steps.ts
@@ -0,0 +1,13 @@
+import {Given,When,Then} from 'cucumber'
+import {assert} from 'chai'
+
+Given('I navigate to google home page', async function () {
+
+  })
+
+  When('I search for mangoes', async function () {
+   
+  })
+
+  Then('I get relevant information', async function () {
+  })
